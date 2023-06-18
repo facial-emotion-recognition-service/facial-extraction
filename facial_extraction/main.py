9d756e173cfde3ff1964bfa31a47322e37a3563a
Or Bartal <orbartal@gmail.com>
2023-06-18 19:07:51 -0400

    Modify main.py

--- facial_extraction/main.py
+++ facial_extraction/main.py
@@ -1,12 +1,17 @@
-import os
-from facial_extraction.core import extraction
+import os, sys
+
+sys.path.append('server')
+sys.path.append('core')
+
+from extractor import Extractor
 import json
 
 if __name__ == "__main__":
-    config_path = os.environ.get("CONFIG_PATH", "config.json")
+    config_path = '../config.json'
 
     with open(config_path, "r") as json_config_file:
         config_data = json.load(json_config_file)
 
-    img_path = "../input_images/8_hap.png"
-    print(extraction.extract_faces(img_path))
+    img_path = "/home/orbartal/Code/files/multi_faces.jpg"
+    extractor = Extractor(config_data)
+    print(extractor.extract_faces(img_path, True))
