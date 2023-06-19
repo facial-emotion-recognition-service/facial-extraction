1fe479cccd6f8b6a59325d4ace33e624303cbab6
Or Bartal <orbartal@gmail.com>
2023-06-19 14:24:16 -0400

    Remove print from server

--- facial_extraction/interface/json_matrix_parser.py
+++ facial_extraction/interface/json_matrix_parser.py
@@ -7,9 +7,5 @@ class JsonMatrixParser:
         pass
 
     def mapMatrixIntoJson(self, matrix_input):
-        # use dump() to write array into json
         json_result = json.dumps(matrix_input, cls=NumpyArrayEncoder)
-        print('start JsonMatrixParser.mapMatrixIntoJson')
-        print(json_result)
-        print('end JsonMatrixParser.mapMatrixIntoJson')
         return json_result
