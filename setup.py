4656e84c3abb3d1fa0a3dfbcb926b1dcc62c5b66
Farid Tabatabaie <farid.t@gmail.com>
2023-07-23 06:12:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(
-    name="human_emotion",
+    name="facial_extraction",
     # version="0.0.1",
-    description="Human Emotion Recognition",
+    description="Face Extraction",
     # license="MIT",
     # author="",
     # author_email="",
     # url="",
     install_requires=requirements,
     packages=find_packages(),
-    # scripts=['human_emotion/scripts/test_script.py'],
+    # scripts=['facial_extraction/scripts/test_script.py'],
     test_suite="tests",
     # include_package_data: to install data from MANIFEST.in
     include_package_data=True,
